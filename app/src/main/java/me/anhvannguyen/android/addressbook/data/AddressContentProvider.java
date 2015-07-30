3194698b356c315c72211e49b9e4ca0512b2189d
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-07-30 16:53:16 -0700

    create content provider file

---
+++ app/src/main/java/me/anhvannguyen/android/addressbook/data/AddressContentProvider.java
@@ -0,0 +1,43 @@
+package me.anhvannguyen.android.addressbook.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+/**
+ * Created by anhvannguyen on 7/30/15.
+ */
+public class AddressContentProvider extends ContentProvider {
+    
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+}
