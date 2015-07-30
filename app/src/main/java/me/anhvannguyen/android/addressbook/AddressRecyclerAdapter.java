d99039428e549688d803316054caf83bcc36e409
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-07-29 20:16:51 -0700

    create adapter for RecyclerView

--- app/src/main/java/me/anhvannguyen/android/addressbook/AddressRecyclerAdapter.java
+++ app/src/main/java/me/anhvannguyen/android/addressbook/AddressRecyclerAdapter.java
@@ -1,7 +1,60 @@
 package me.anhvannguyen.android.addressbook;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 /**
  * Created by anhvannguyen on 7/29/15.
  */
-public class AddressRecyclerAdapter {
+public class AddressRecyclerAdapter extends RecyclerView.Adapter<AddressRecyclerAdapter.ViewHolder> {
+    private Cursor mCursor;
+    private Context mContext;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mAddressTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mAddressTextView = (TextView) itemView.findViewById(R.id.address_item_name_textview);
+        }
+    }
+
+    public AddressRecyclerAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.address_list_item, parent, false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mCursor == null) {
+            return 0;
+        }
+        return mCursor.getCount();
+    }
+
+    public void swapCursor(Cursor newCursor) {
+        mCursor = newCursor;
+        notifyDataSetChanged();
+    }
+
+    public Cursor getCursor() {
+        return mCursor;
+    }
 }
