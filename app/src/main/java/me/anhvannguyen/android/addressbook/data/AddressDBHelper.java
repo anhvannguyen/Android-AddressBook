52dd5018ae81f7ad4d972bee1c1c1e5462e14d37
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-07-30 15:07:52 -0700

    create db table for address

--- app/src/main/java/me/anhvannguyen/android/addressbook/data/AddressDBHelper.java
+++ app/src/main/java/me/anhvannguyen/android/addressbook/data/AddressDBHelper.java
@@ -18,11 +18,26 @@ public class AddressDBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_ADDRESS_TABLE = "CREATE TABLE " + AddressContract.AddressEntry.TABLE_NAME + " (" +
+                AddressContract.AddressEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                AddressContract.AddressEntry.COLUMN_ALIAS + " TEXT, " +
+                AddressContract.AddressEntry.COLUMN_FIRST_NAME + " TEXT, " +
+                AddressContract.AddressEntry.COLUMN_MIDDLE_NAME + " TEXT, " +
+                AddressContract.AddressEntry.COLUMN_LAST_NAME + " TEXT, " +
+                AddressContract.AddressEntry.COLUMN_PHONE + " TEXT, " +
+                AddressContract.AddressEntry.COLUMN_EMAIL + " TEXT, " +
+                AddressContract.AddressEntry.COLUMN_STREET + " TEXT, " +
+                AddressContract.AddressEntry.COLUMN_STATE + " TEXT, " +
+                AddressContract.AddressEntry.COLUMN_ZIPCODE + " INTEGER" +
+                ")";
 
+
+        db.execSQL(SQL_CREATE_ADDRESS_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + AddressContract.AddressEntry.TABLE_NAME);
+        onCreate(db);
     }
 }
