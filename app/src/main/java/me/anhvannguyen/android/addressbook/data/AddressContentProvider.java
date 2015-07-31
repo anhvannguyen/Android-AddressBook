5d20f9fd0108e453f7b62623fd3186a492a1958b
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-07-30 18:46:40 -0700

    create uri matcher

--- app/src/main/java/me/anhvannguyen/android/addressbook/data/AddressContentProvider.java
+++ app/src/main/java/me/anhvannguyen/android/addressbook/data/AddressContentProvider.java
@@ -2,6 +2,7 @@ package me.anhvannguyen.android.addressbook.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -9,11 +10,25 @@ import android.net.Uri;
  * Created by anhvannguyen on 7/30/15.
  */
 public class AddressContentProvider extends ContentProvider {
-    
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private AddressDBHelper mOpenHelper;
+
+    private static final int ADDRESS = 100;
+    private static final int ADDRESS_WITH_ID = 101;
 
     @Override
     public boolean onCreate() {
         return false;
+    }
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = AddressContract.CONTENT_AUTHORITY;
+
+        matcher.addURI(authority, AddressContract.PATH_ADDRESS, ADDRESS);
+        matcher.addURI(authority, AddressContract.PATH_ADDRESS + "/#", ADDRESS_WITH_ID);
+        
+        return matcher;
     }
 
     @Override
