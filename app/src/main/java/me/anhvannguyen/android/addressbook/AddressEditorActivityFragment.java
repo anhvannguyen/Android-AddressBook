31334eeedeeb3fd7337229d76df89d3a7a4c6ebb
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-07-31 16:19:53 -0700

    create editor activity and change layout to GridLayout

---
+++ app/src/main/java/me/anhvannguyen/android/addressbook/AddressEditorActivityFragment.java
@@ -0,0 +1,23 @@
+package me.anhvannguyen.android.addressbook;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class AddressEditorActivityFragment extends Fragment {
+
+    public AddressEditorActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_address_editor, container, false);
+    }
+}
