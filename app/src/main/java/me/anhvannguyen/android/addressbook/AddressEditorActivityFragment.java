aa4e75219de343dabace0193247a506df80e8517
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-07-31 20:57:51 -0700

    assign view from layout

--- app/src/main/java/me/anhvannguyen/android/addressbook/AddressEditorActivityFragment.java
+++ app/src/main/java/me/anhvannguyen/android/addressbook/AddressEditorActivityFragment.java
@@ -1,16 +1,38 @@
 package me.anhvannguyen.android.addressbook;
 
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class AddressEditorActivityFragment extends Fragment {
+    private TextInputLayout mNameInputLayout;
+    private TextInputLayout mPhoneInputLayout;
+    private TextInputLayout mEmailInputLayout;
+    private TextInputLayout mStreetInputLayout;
+    private TextInputLayout mCityInputLayout;
+    private TextInputLayout mStateInputLayout;
+    private TextInputLayout mZipInputLayout;
+
+    private EditText mNameEditText;
+    private EditText mPhoneEditText;
+    private EditText mEmailEditText;
+    private EditText mStreetEditText;
+    private EditText mCityEditText;
+    private EditText mStateEditText;
+    private EditText mZipEditText;
+
+    private Button mSaveButton;
 
     public AddressEditorActivityFragment() {
     }
@@ -18,6 +40,34 @@ public class AddressEditorActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_address_editor, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_address_editor, container, false);
+
+        mNameInputLayout = (TextInputLayout) rootView.findViewById(R.id.name_textinputlayout);
+        mPhoneInputLayout = (TextInputLayout) rootView.findViewById(R.id.phone_textinputlayout);
+        mEmailInputLayout = (TextInputLayout) rootView.findViewById(R.id.email_textinputlayout);
+        mStreetInputLayout = (TextInputLayout) rootView.findViewById(R.id.street_textinputlayout);
+        mCityInputLayout = (TextInputLayout) rootView.findViewById(R.id.city_textinputlayout);
+        mStateInputLayout = (TextInputLayout) rootView.findViewById(R.id.state_textinputlayout);
+        mZipInputLayout = (TextInputLayout) rootView.findViewById(R.id.zip_textinputlayout);
+
+        mNameEditText = (EditText) rootView.findViewById(R.id.name_edittext);
+        mPhoneEditText = (EditText) rootView.findViewById(R.id.phone_edittext);
+        mEmailEditText = (EditText) rootView.findViewById(R.id.email_edittext);
+        mStreetEditText = (EditText) rootView.findViewById(R.id.street_edittext);
+        mCityEditText = (EditText) rootView.findViewById(R.id.city_edittext);
+        mStateEditText = (EditText) rootView.findViewById(R.id.state_edittext);
+        mZipEditText = (EditText) rootView.findViewById(R.id.zip_edittext);
+
+        mPhoneEditText.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+
+        mSaveButton = (Button) rootView.findViewById(R.id.save_button);
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(rootView, "Save Works!", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+        return rootView;
     }
 }
