8a2024711ce01b354f743b5ce1e02d163fc8cdf4
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-08-01 18:18:59 -0700

    use Loader to load data for RecyclerView

--- app/src/main/java/me/anhvannguyen/android/addressbook/MainActivityFragment.java
+++ app/src/main/java/me/anhvannguyen/android/addressbook/MainActivityFragment.java
@@ -1,22 +1,30 @@
 package me.anhvannguyen.android.addressbook;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import me.anhvannguyen.android.addressbook.data.AddressContract;
+
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final int ADDRESS_LIST_LOADER = 0;
+
     private RecyclerView mAddressRecyclerView;
     private AddressRecyclerAdapter mRecycleAdapter;
     private FloatingActionButton mAddButton;
@@ -53,5 +61,39 @@ public class MainActivityFragment extends Fragment {
         mAddressRecyclerView.setAdapter(mRecycleAdapter);
         
         return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(ADDRESS_LIST_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getLoaderManager().restartLoader(ADDRESS_LIST_LOADER, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(
+                getActivity(),
+                AddressContract.AddressEntry.CONTENT_URI,
+                null,
+                null,
+                null,
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mRecycleAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mRecycleAdapter.swapCursor(null);
     }
 }
