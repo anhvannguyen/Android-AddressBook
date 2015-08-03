c3eeb9686c796357aba2c1a98868d0d3e4bf19fc
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-08-02 22:07:09 -0700

    set up views and Loader

--- app/src/main/java/me/anhvannguyen/android/addressbook/DetailActivityFragment.java
+++ app/src/main/java/me/anhvannguyen/android/addressbook/DetailActivityFragment.java
@@ -1,16 +1,56 @@
 package me.anhvannguyen.android.addressbook;
 
-import android.support.v4.app.Fragment;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import me.anhvannguyen.android.addressbook.data.AddressContract;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class DetailActivityFragment extends Fragment {
+public class DetailActivityFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final int ADDRESS_DETAIL_LOADER = 0;
+    private Uri mUri;
+
+    private static final String[] ADDRESS_DETAIL_PROJECTION = {
+            AddressContract.AddressEntry._ID,
+            AddressContract.AddressEntry.COLUMN_NAME,
+            AddressContract.AddressEntry.COLUMN_PHONE,
+            AddressContract.AddressEntry.COLUMN_EMAIL,
+            AddressContract.AddressEntry.COLUMN_STREET,
+            AddressContract.AddressEntry.COLUMN_CITY,
+            AddressContract.AddressEntry.COLUMN_STATE,
+            AddressContract.AddressEntry.COLUMN_ZIPCODE
+    };
+
+    public static final int COL_ADDRESS_ID = 0;
+    public static final int COL_ADDRESS_NAME = 1;
+    public static final int COL_ADDRESS_PHONE = 2;
+    public static final int COL_ADDRESS_EMAIL = 3;
+    public static final int COL_ADDRESS_STREET = 4;
+    public static final int COL_ADDRESS_CITY = 5;
+    public static final int COL_ADDRESS_STATE = 6;
+    public static final int COL_ADDRESS_ZIP = 7;
+
+
+    private TextView mNameTextView;
+    private TextView mPhoneTextView;
+    private TextView mEmailTextView;
+    private TextView mStreetTextView;
+    private TextView mCityTextView;
+    private TextView mStateTextView;
+    private TextView mZipTextView;
+
 
     public DetailActivityFragment() {
     }
@@ -18,6 +58,50 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        mNameTextView = (TextView) rootView.findViewById(R.id.name_detail_textview);
+        mPhoneTextView = (TextView) rootView.findViewById(R.id.phone_detail_textview);
+        mEmailTextView = (TextView) rootView.findViewById(R.id.email_detail_textview);
+        mStreetTextView = (TextView) rootView.findViewById(R.id.street_detail_textview);
+        mCityTextView = (TextView) rootView.findViewById(R.id.city_detail_textview);
+        mStateTextView = (TextView) rootView.findViewById(R.id.state_detail_textview);
+        mZipTextView = (TextView) rootView.findViewById(R.id.zip_detail_textview);
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(ADDRESS_DETAIL_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        if (mUri != null) {
+            return new CursorLoader(
+                    getActivity(),
+                    mUri,
+                    ADDRESS_DETAIL_PROJECTION,
+                    null,
+                    null,
+                    null
+            );
+        }
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (!data.moveToFirst()) {
+            return;
+        }
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
