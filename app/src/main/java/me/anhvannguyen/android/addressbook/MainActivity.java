33e399968a1eaa83e7f24f4175474ed517666787
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-08-13 00:08:38 -0700

    add fragment to tablet view and implement callback for detail fragment

--- app/src/main/java/me/anhvannguyen/android/addressbook/MainActivity.java
+++ app/src/main/java/me/anhvannguyen/android/addressbook/MainActivity.java
@@ -1,19 +1,53 @@
 package me.anhvannguyen.android.addressbook;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements MainActivityFragment.AddressSelectedCallback {
+    private boolean mTwoPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.address_detail_container) != null) {
+            // If this view is present, then the activity is in two-pane mode.
+            mTwoPane = true;
+
+            if (savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.address_detail_container, new DetailActivityFragment())
+                        .commit();
+            }
+        } else {
+            mTwoPane = false;
+        }
     }
 
+    @Override
+    public void onItemSelected(Uri addressUri) {
+        if (mTwoPane) {
+            Bundle args = new Bundle();
+            args.putParcelable(DetailActivityFragment.DETAIL_URI, addressUri);
+
+            DetailActivityFragment fragment = new DetailActivityFragment();
+            fragment.setArguments(args);
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.address_detail_container, fragment)
+                    .commit();
+        } else {
+            Intent intent = new Intent(this, DetailActivity.class)
+                    .setData(addressUri);
+            startActivity(intent);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
