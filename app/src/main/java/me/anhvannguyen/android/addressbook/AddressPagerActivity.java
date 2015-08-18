e60fe74a234d8d85b06fa5cb7f409516ff08f4b6
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-08-18 01:21:11 -0700

    create activity for view pager

--- app/src/main/java/me/anhvannguyen/android/addressbook/AddressPagerActivity.java
+++ app/src/main/java/me/anhvannguyen/android/addressbook/AddressPagerActivity.java
@@ -1,7 +1,41 @@
 package me.anhvannguyen.android.addressbook;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+
 /**
  * Created by anhvannguyen on 8/18/15.
  */
-public class AddressPagerActivity {
+public class AddressPagerActivity extends ActionBarActivity {
+    private ViewPager mAddressViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address_pager);
+
+        mAddressViewPager = (ViewPager) findViewById(R.id.activity_address_detail_pager);
+        mAddressViewPager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                Bundle arguments = new Bundle();
+                arguments.putParcelable(DetailActivityFragment.ADDRESS_DETAIL_URI, getIntent().getData());
+
+                DetailActivityFragment fragment = new DetailActivityFragment();
+                fragment.setArguments(arguments);
+                
+                return fragment;
+            }
+
+            @Override
+            public int getCount() {
+                return 0;
+            }
+        });
+
+
+    }
 }
